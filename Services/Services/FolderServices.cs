9dce20294a38f0f76d8a32537be6ce2c98117645
Victor de la Rosa <vrosa@moneytrans.eu>
2017-03-27 16:35:01 +0200

    Added directory control in Folder Services

--- Services/Services/FolderServices.cs
+++ Services/Services/FolderServices.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using AutoMapper;
 using DocumentManager.Persistence.Daos;
 using DocumentManager.Persistence.Models;
 using DocumentManager.Services.Dtos;
 using DocumentManager.Services.Exceptions;
+using Microsoft.Extensions.Configuration;
 
 namespace DocumentManager.Services.Services
 {
 	public class FolderServices : AbstractServices<FolderDao>
 	{
-		public FolderServices() : base(new FolderDao()) { }
+		readonly string repoPath;
+
+		public FolderServices() : base(new FolderDao())
+		{
+			var configurationBuilder = new ConfigurationBuilder()
+				 .AddJsonFile("repo_settings.json");
+			var configuration = configurationBuilder.Build();
+
+			repoPath = $"{configuration["RepositoryPath"]}"; }
 
 		public long Create(string securityToken, FolderDto folder)
 		{
@@ -20,6 +30,13 @@ namespace DocumentManager.Services.Services
 				if (similarName != null)
 					throw new DocumentManagerException(DocumentManagerException.FOLDER_ALREADY_EXISTS,
 													   "You already have a folder with this name in this directory");
+				if (folder.IdFolderRoot.HasValue)
+				{
+					var rootFolder = _dao.Read(folder.IdFolderRoot.Value);
+					if (rootFolder == null)
+						throw new DocumentManagerException(DocumentManagerException.FOLDER_NOT_FOUND,
+														   "Folder with id " + folder.Id + " does not exist");
+				}
 				var folderDom = Mapper.Map<Folder>(folder);
 				using (var sessionService = new SessionServices())
 				{
@@ -28,6 +45,7 @@ namespace DocumentManager.Services.Services
 				}
 				folderDom = _dao.Create(folderDom);
 
+				Directory.CreateDirectory(GetFullPath(folderDom));
 				//TODO Audit
 				return folderDom.Id;
 			}
@@ -60,9 +78,13 @@ namespace DocumentManager.Services.Services
 				if (similarName != null)
 					throw new DocumentManagerException(DocumentManagerException.FOLDER_ALREADY_EXISTS,
 													   "You already have a folder with this name in this directory");
-				//TODO Audit
+				string previousPath = GetFullPath(folderDom);
 				Mapper.Map(folder, folderDom);
 				_dao.Update(folderDom);
+				Directory.Move(previousPath, GetFullPath(folderDom));
+				Directory.Delete(previousPath);
+				//TODO Audit
+
 			}
 			catch (DocumentManagerException)
 			{
@@ -89,8 +111,10 @@ namespace DocumentManager.Services.Services
 						throw new DocumentManagerException(DocumentManagerException.UNAUTHORIZED,
 														   "You do not have permissions to update this folder");
 				}
-				//TODO Audit
+				string path = GetFullPath(folderDom);
 				_dao.Delete(folderDom);
+				Directory.Delete(path);
+				//TODO Audit
 			}
 			catch (DocumentManagerException)
 			{
@@ -122,5 +146,31 @@ namespace DocumentManager.Services.Services
 				throw new DocumentManagerException(DocumentManagerException.ERROR_DOCUMENT_MANAGER_SERVER, e.Message, e);
 			}
 		}
+
+		public string GetFullPath(long idFolder)
+		{
+			var folder = _dao.Read(idFolder);
+			if (folder == null)
+				throw new DocumentManagerException(DocumentManagerException.FOLDER_NOT_FOUND,
+					 "Folder with id " + idFolder + " does not exist");
+			return GetFullPath(_dao.Read(idFolder));
+		}
+
+		#region Private Methods
+
+		string GetFullPath(Folder folder)
+		{
+			var _folder = folder;
+			string path = "";
+			while (_folder.IdFolderRoot.HasValue)
+			{
+				path = folder.Name + "/" + path;
+				_folder = _folder.IdFolderRootNavigation;
+			}
+			path = _folder.Name + "/" + path;
+			return repoPath + path;
+		}
+
+		#endregion
 	}
 }
