c1ce7375be9a2c91d2964763acd33ad3c4c29520
Victor de la Rosa <vrosa@moneytrans.eu>
2017-03-28 12:03:21 +0200

    All mappings included in AutoMapperConfig

--- Services/Mapping/AutoMapperConfig.cs
+++ Services/Mapping/AutoMapperConfig.cs
@@ -1,21 +1,31 @@
 ﻿using AutoMapper;
 using DocumentManager.Persistence.Models;
 using DocumentManager.Services.Dtos;
+using DocumentManager.Services.Filters;
 
 namespace DocumentManager.Services.Mapping
 {
 	public static class AutoMapperConfig
 	{
-        static bool _registered = false;
+        static bool _registered;
 
 		public static void RegisterMappings()
 		{
             if (_registered) return;
 
+			_registered = true;
+
 			Mapper.Initialize(cfg =>
 			{
 				cfg.CreateMap<User, UserDto>().ReverseMap();
-                cfg.CreateMap<Session, SessionDto>();
+				cfg.CreateMap<Session, SessionDto>().ReverseMap();
+				cfg.CreateMap<Group, GroupDto>().ReverseMap();
+				cfg.CreateMap<UserGroup, UserGroupDto>().ReverseMap();
+				cfg.CreateMap<Folder, FolderDto>().ReverseMap();
+				cfg.CreateMap<Document, DocumentDto>().ReverseMap();
+				cfg.CreateMap<Audit, AuditDto>().ReverseMap();
+
+				cfg.CreateMap<AuditFilterDto, AuditFilterDto>();
 			});
 		}
 	}
