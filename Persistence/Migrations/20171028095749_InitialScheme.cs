1fee62ee43d3ad4ff7256d580f265db2360cfa32
Victor de la Rosa <vrosa@moneytrans.eu>
2017-10-28 12:10:03 +0200

    Initial migration created

---
+++ Persistence/Migrations/20171028095749_InitialScheme.cs
@@ -0,0 +1,253 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FileSharing.Persistence.Migrations
+{
+    public partial class InitialScheme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Audit",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Action = table.Column<int>(type: "varchar(50)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IdObject = table.Column<string>(type: "varchar(50)", nullable: false),
+                    IdUser = table.Column<long>(type: "bigint", nullable: false),
+                    Object = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Audit", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    LastName = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Login = table.Column<string>(type: "varchar(50)", nullable: false),
+                    Password = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Folder",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IdFolderRoot = table.Column<long>(type: "bigint", nullable: true),
+                    IdUser = table.Column<long>(type: "bigint", nullable: false),
+                    Name = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Folder", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Folder_Folder",
+                        column: x => x.IdFolderRoot,
+                        principalTable: "Folder",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Folder_User",
+                        column: x => x.IdUser,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Group",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IdAdmin = table.Column<long>(type: "bigint", nullable: false),
+                    Name = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Group", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Group_User",
+                        column: x => x.IdAdmin,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Session",
+                columns: table => new
+                {
+                    SecurityToken = table.Column<string>(type: "varchar(200)", nullable: false),
+                    DateLastAccess = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IdUser = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Session", x => x.SecurityToken);
+                    table.ForeignKey(
+                        name: "FK_Session_User",
+                        column: x => x.IdUser,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "File",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ContentType = table.Column<string>(type: "varchar(50)", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Filename = table.Column<string>(type: "varchar(200)", nullable: false),
+                    IdFolder = table.Column<long>(type: "bigint", nullable: true),
+                    IdGroup = table.Column<long>(type: "bigint", nullable: true),
+                    IdUser = table.Column<long>(type: "bigint", nullable: false),
+                    IsPublic = table.Column<bool>(type: "bit", nullable: false),
+                    ModificationDate = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_File", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_File_Folder",
+                        column: x => x.IdFolder,
+                        principalTable: "Folder",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_File_Group",
+                        column: x => x.IdGroup,
+                        principalTable: "Group",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_File_User",
+                        column: x => x.IdUser,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGroup",
+                columns: table => new
+                {
+                    IdUser = table.Column<long>(type: "bigint", nullable: false),
+                    IdGroup = table.Column<long>(type: "bigint", nullable: false),
+                    DateInclusionApproval = table.Column<DateTime>(type: "datetime", nullable: true),
+                    DateInclusionRequest = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGroup", x => new { x.IdUser, x.IdGroup });
+                    table.ForeignKey(
+                        name: "FK_UserGroup_Group",
+                        column: x => x.IdGroup,
+                        principalTable: "Group",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserGroup_User",
+                        column: x => x.IdUser,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_File_IdFolder",
+                table: "File",
+                column: "IdFolder");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_File_IdGroup",
+                table: "File",
+                column: "IdGroup");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_File_IdUser",
+                table: "File",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Folder_IdFolderRoot",
+                table: "Folder",
+                column: "IdFolderRoot");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Folder_IdUser",
+                table: "Folder",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Group_IdAdmin",
+                table: "Group",
+                column: "IdAdmin");
+
+            migrationBuilder.CreateIndex(
+                name: "UK_Group",
+                table: "Group",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Session_IdUser",
+                table: "Session",
+                column: "IdUser");
+
+            migrationBuilder.CreateIndex(
+                name: "UK_User",
+                table: "User",
+                column: "Login",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGroup_IdGroup",
+                table: "UserGroup",
+                column: "IdGroup");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Audit");
+
+            migrationBuilder.DropTable(
+                name: "File");
+
+            migrationBuilder.DropTable(
+                name: "Session");
+
+            migrationBuilder.DropTable(
+                name: "UserGroup");
+
+            migrationBuilder.DropTable(
+                name: "Folder");
+
+            migrationBuilder.DropTable(
+                name: "Group");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
