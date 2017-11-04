d1bb238e4b180ecce9557b1b80a2318ed2551184
Victor de la Rosa <vrosa@moneytrans.eu>
2017-11-04 04:07:07 +0100

    Last fixes in vagrant-deploy

--- vagrant-deploy/provision/bootstrap.sh
+++ vagrant-deploy/provision/bootstrap.sh
@@ -5,9 +5,8 @@ sudo apt-get -y install nginx
 sudo service nginx start
 
 # set up nginx server
-sudo cp /vagrant/provision/nginx/default /etc/nginx/sites-available/default
-sudo chmod 644 /etc/nginx/sites-available/default
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo cp /vagrant/provision/nginx/default /etc/nginx/sites-enabled/default
+sudo chmod 644 /etc/nginx/sites-enabled/default
 sudo service nginx restart
 
 # clean /var/www
@@ -21,15 +20,15 @@ sudo apt-get update
 sudo apt-get -y install curl libunwind8 gettext apt-transport-https --force-yes
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-jessie-prod jessie main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
 sudo apt-get -y install dotnet-sdk-2.0.2
 
 #deploy FileSharingWeb
-sudo mkdir -p /var/www/filesharing/{app,logs,folders}
-sudo cp -R /vagrant/provision/FileSharing/ /var/www/filesharing/app/
-sudo cp /vagrant/provision/FileSharingConfig/* /var/www/filesharing/app/
-sudo chown -R www-data:www-data /var/www/filesharing
+sudo mkdir -p /var/filesharing/{app,logs,folders}
+sudo cp -r /vagrant/provision/FileSharing/* /var/filesharing/app/
+sudo cp /vagrant/provision/FileSharingConfig/* /var/filesharing/app/
+sudo chown -R www-data:www-data /var/filesharing
 sudo cp /vagrant/provision/systemd/filesharing.service /etc/systemd/system/filesharing.service
 sudo systemctl enable filesharing.service
 sudo systemctl start filesharing.service
